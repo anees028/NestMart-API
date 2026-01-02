be0bb8e7c384b3403f5aafc6bb66ea4a585ad9da
anees028 <rehmananees028@gmail.com>
2026-01-02 16:13:30 +0100

    Enabling global validation.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  // ENABLE VALIDATION GLOBALLY
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Strips out unwanted properties (security!)
+    forbidNonWhitelisted: true, // Throws error if unwanted properties are sent
+  }));
+
+
 }
 bootstrap();

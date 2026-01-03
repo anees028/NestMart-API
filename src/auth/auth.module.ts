5dd4a2b39678edd64baa41ed9bc1af6d867889ce
anees028 <rehmananees028@gmail.com>
2026-01-03 19:11:40 +0100

    Added a protected new route, which execute after checking wether the user is authorized to see the profile or not.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -10,10 +11,10 @@ import { JwtModule } from '@nestjs/jwt';
     JwtModule.register({
       global: true, // Makes the JWT service available everywhere
       secret: 'superSecretKey123', // IN PRODUCTION: Use process.env.JWT_SECRET
-      signOptions: { expiresIn: '1h' }, // Token expires in 1 hour
+      signOptions: { expiresIn: '60m' }, // Token expires in 1 hour
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}

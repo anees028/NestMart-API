17a7d50455db009ceed91b14bb82d9157aeb739f
anees028 <rehmananees028@gmail.com>
2026-01-13 22:01:45 +0100

    Microservice added for the specific module to be used as after creating order.

--- apps/nest-mart_api/src/app.module.ts
+++ apps/nest-mart_api/src/app.module.ts
@@ -10,7 +10,6 @@ import { Product } from './products/entities/product.entity';
 import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 import { ConfigModule } from '@nestjs/config';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -28,20 +27,6 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
       synchronize: true,          // CRITICAL: Auto-creates tables. Set to FALSE in production!
       autoLoadEntities: true,
     }),
-    ClientsModule.register([
-      {
-        name: 'KAFKA_SERVICE', // We will inject this name later
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: ['localhost:9092'],
-          },
-          consumer: {
-            groupId: 'api-gateway-consumer',
-          },
-        },
-      },
-    ]),
     UsersModule,
     AuthModule,
     ProductsModule,
--- apps/nest-mart_api/src/orders/orders.module.ts
+++ apps/nest-mart_api/src/orders/orders.module.ts
@@ -4,9 +4,26 @@ import { OrdersController } from './orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Product } from '../products/entities/product.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Product])],
+  imports: [
+    TypeOrmModule.forFeature([Order, Product]),
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE', // We will inject this name later
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'api-gateway-consumer',
+          },
+        },
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })

38dc0be082eabad297137be64ca55ad70dd96516
anees028 <rehmananees028@gmail.com>
2026-01-13 22:02:47 +0100

    Injected a microservice within order.service.ts

--- apps/nest-mart_api/src/orders/orders.service.ts
+++ apps/nest-mart_api/src/orders/orders.service.ts
@@ -1,13 +1,11 @@
-import {
-  BadRequestException,
-  Injectable,
-} from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, In, DataSource } from 'typeorm'; // Import 'In' operator
 import { Order } from './entities/order.entity';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { Product } from '../products/entities/product.entity';
 import { User } from '../users/user.entity';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Injectable()
 export class OrdersService {
@@ -15,7 +13,14 @@ export class OrdersService {
     @InjectRepository(Order)
     private orderRepo: Repository<Order>,
     private dataSource: DataSource,
+    // Inject the Kafka Client
+    @Inject('KAFKA_SERVICE') private kafkaClient: ClientKafka,
   ) {}
+
+  // 1. Connect to Kafka when the app starts
+  async onModuleInit() {
+    await this.kafkaClient.connect();
+  }
 
   async create(createOrderDto: CreateOrderDto, user: User) {
     // initialize a query runner for transaction query management
@@ -65,6 +70,17 @@ export class OrdersService {
 
       // 3. If we got here, everything is good. PERMANENTLY SAVE.
       await queryRunner.commitTransaction();
+
+      // 2. FIRE AND FORGET
+      // We don't wait for this. We just throw it into Kafka and return.
+      const eventData = {
+        orderId: savedOrder.id,
+        userEmail: user.email,
+        totalPrice: savedOrder.id * 100, // Id is multiplied by 100 just for demo purposes
+      };
+
+      this.kafkaClient.emit('order_created', eventData);
+
       return savedOrder;
     } catch (err) {
       // 4. If ANY error happened above (Out of stock, DB crash, etc.)

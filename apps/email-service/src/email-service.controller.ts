15daabea32d95c7add65b5a66d57366476cf97a8
anees028 <rehmananees028@gmail.com>
2026-01-13 21:11:54 +0100

    Email controller added for the email microservice.

--- apps/email-service/src/email-service.controller.ts
+++ apps/email-service/src/email-service.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { EmailServiceService } from './email-service.service';
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class EmailServiceController {
-  constructor(private readonly emailServiceService: EmailServiceService) {}
-
-  @Get()
-  getHello(): string {
-    return this.emailServiceService.getHello();
+  
+  // "order_created" is the TOPIC name we will listen to
+  @EventPattern('order_created')
+  handleOrderCreated(@Payload() data: any) {
+    // In real life, you would use a library like 'nodemailer' here
+    console.log('------------------------------------------------');
+    console.log('📧 EMAIL SERVICE: Received Order Event!');
+    console.log(`To: ${data.userEmail}`);
+    console.log(`Subject: Order #${data.orderId} Confirmed`);
+    console.log('------------------------------------------------');
   }
 }
--- apps/email-service/src/main.ts
+++ apps/email-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { EmailServiceModule } from './email-service.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(EmailServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    EmailServiceModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092'], // Connect to Kafka Docker
+        },
+        consumer: {
+          groupId: 'email-consumer', // Unique ID for this worker group
+        },
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
